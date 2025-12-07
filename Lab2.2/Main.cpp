10102c21c89ba0b35cebe9966b9a5781eab6fdca
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-07 16:17:55 +0700

    Update Main.cpp

--- Lab2.2/Main.cpp
+++ Lab2.2/Main.cpp
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
-void caculator(int *number,float *unitprice , int *quantity,float *totolcost,float *grandTotal);
+void caculator (int *number, float *unitprice, int *quantity, float *totolcost, float *grandTotal);
 
-int main(){
-    int n,qtt;
-    float up,tc,gt = 0.0;
-    
+int main () {
+    int n, qtt;
+    float up, tc, gt = 0.0;
+
     printf("Plsese Fill How many unit you buy: ");
-    if(scanf("%d",&n) != 1){
+    if (scanf("%d", &n) != 1) {
         printf("Error Plsese try again.");
         return 1;
     }
-
-    caculator(&n,&up,&qtt,&tc,&gt);
-
-    printf("Grand Total: %.2f\n",gt);
+    caculator(&n, &up, &qtt, &tc, &gt);
+    printf("Grand Total: %.2f\n", gt);
 }
 
-void caculator(int *number,float *unitprice , int *quantity,float *totolcost,float *grandTotal){
-    for(int i = 0 ; i < *number ; i++){
-        printf("Plsese Fill unitprice and quantity [%d]: ",i+1);
-        scanf("%f %d",unitprice,quantity);
+void caculator(int *number, float *unitprice, int *quantity, float *totolcost, float *grandTotal) {
+    for (int i = 0; i < *number; i++) {
+        printf("Plsese Fill unitprice and quantity [%d]: ", i + 1);
+        scanf("%f %d", unitprice, quantity);
 
-        if (*unitprice >= 1000.00){
+        if (*unitprice >= 1000.00) {
             *totolcost = (*unitprice * *quantity) * 0.90;
         } else {
             *totolcost = *unitprice * *quantity;
-        } 
-        
+        }
         *grandTotal += *totolcost;
     }
 }
