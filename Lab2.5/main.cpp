0d716e60a5fa4446abf59f38e0d442eb2e6cc60f
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-07 19:27:59 +0700

    Add files via upload

---
+++ Lab2.5/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main () {
+    int score , i = 0;
+    int CountA = 0 , CountB = 0 , CountC = 0 , CountD = 0 , CountF = 0;
+    while (score != -1) {
+        
+        printf("Enter your Score [%d]: ",i+1);
+        if (scanf("%d",&score) != 1) {
+            printf("Pleses try again");
+            break;
+        } 
+        
+        if (score >= 80) {
+            CountA += 1;
+        } else if (score >= 70) {
+            CountB += 1;
+        } else if (score >= 60) {
+            CountC += 1;
+        } else if (score >= 50) {
+            CountD += 1;
+        } else if (score < 50 && score != (-1)) {
+            CountF +=1;
+        } i++;
+    }
+
+    if (score == -1) {
+        printf("Grade A Count: %d\n",CountA);
+        printf("Grade B Count: %d\n",CountB);
+        printf("Grade C Count: %d\n",CountC);
+        printf("Grade D Count: %d\n",CountD);
+        printf("Grade F Count: %d\n",CountF);
+    }
+    return 0;
+}
