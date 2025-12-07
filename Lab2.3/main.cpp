9e86fea24c07cafcebd7ab8852793f48dba2c5cb
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-07 16:23:28 +0700

    Update main.cpp

--- Lab2.3/main.cpp
+++ Lab2.3/main.cpp
@@ -1,39 +1,31 @@
 #include <stdio.h>
 
-void invest(int *termCode, float *investmentAmount, float *Amount);
+void invest (int *termCode, float *investmentAmount, float *Amount);
 
-int main()
-{
+int main () {
     int tC;
     float imA, aM;
 
     printf("plsese input termCode and investmentAmount: ");
-    if (scanf("%d%f", &tC, &imA) != 2)
-    {
+    if (scanf("%d%f", &tC, &imA) != 2) {
         printf("Erorr Pleses Try Again");
         return 1;
     }
 
     invest(&tC, &imA, &aM);
-
     return 0;
 }
 
-void invest(int *termCode, float *investmentAmount, float *Amount)
-{
+void invest (int *termCode, float *investmentAmount, float *Amount) {
 
-    switch (*termCode)
-    {
-    case 1:
-    {
-        if (*investmentAmount < 5000)
-        {
+    switch (*termCode) {
+    case 1: {
+        if (*investmentAmount < 5000) {
             float vat = 0.03;
             *Amount = *investmentAmount * vat;
             printf("%.2f", *Amount);
         }
-        else if (*investmentAmount >= 5000)
-        {
+        else if (*investmentAmount >= 5000) {
             float vat = 0.04;
             *Amount = *investmentAmount * vat;
             printf("%.2f", *Amount);
@@ -41,16 +33,13 @@ void invest(int *termCode, float *investmentAmount, float *Amount)
     }
     break;
 
-    case 2:
-    {
-        if (*investmentAmount < 10000)
-        {
+    case 2: {
+        if (*investmentAmount < 10000) {
             float vat = 0.05;
             *Amount = *investmentAmount * vat;
             printf("%.2f", *Amount);
         }
-        else if (*investmentAmount >= 5000)
-        {
+        else if (*investmentAmount >= 5000) {
             float vat = 0.065;
             *Amount = *investmentAmount * vat;
             printf("%.2f", *Amount);
@@ -58,18 +47,18 @@ void invest(int *termCode, float *investmentAmount, float *Amount)
     }
     break;
 
-    case 3:
-    {
+    case 3: {
         float vat = 0.08;
         *Amount = *investmentAmount * vat;
         printf("%.2f", *Amount);
     }
     break;
 
-    default:
-    {
+    default: {
         printf("Invalid Term Code");
     }
     break;
     }
 }
+
+//becuse i need to try to use funtion.make me never fogot how to use.
