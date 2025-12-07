cf92d0e5140e25181af5bcea1384f799717eb297
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-07 16:19:58 +0700

    Update Main.c

--- Lab2.1/Main.c
+++ Lab2.1/Main.c
@@ -1,12 +1,2 @@
 #include <stdio.h>
-
-int main () {
-    int n;
-
-    printf("Pls Fill Number: ");
-    scanf("%d",&n);
-
-    for (int i = 0; i < n; i++){
-        printf("[%d] Hello world\n",i+1);
-    }
-}
+int main () { int n; printf("Pls Fill Number: "); scanf("%d",&n); for (int i = 0; i < n; i++){ printf("[%d] Hello world\n",i+1); } }
