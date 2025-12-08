ccf8b2e69db5e28ca1410a1566e5757e91bee23e
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-08 15:57:31 +0700

    Add files via upload

---
+++ Lab2.6/Main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void money(int *N, int *n , int *s , float *dD , float *mT,float *cD);
+
+int main() {
+    int N_MONTHS,month,successCount = 0;
+    float dailyDeposit,monthlyTotal,currentDeposit;
+    
+    if (scanf("%d", &N_MONTHS) != 1) {
+        return 1;
+    }
+
+    money(&N_MONTHS, &month, &successCount, &dailyDeposit, &monthlyTotal,&currentDeposit);
+
+    printf("Success Count: %d\n", successCount);
+    
+    return 0;
+}
+
+void money(int *NM, int *m, int *s, float *dD, float *mT,float *cD) {
+
+    for (int i = 0; i < *NM; i++) {
+        *mT = 0.0;
+
+        while (1) {
+            if (scanf("%f", &*cD) != 1) { 
+                return; 
+            }
+
+            if (*cD == 0.00) {  //ถ้าเจอ 0.00 ให้หยุด loop ข้างในเเละออกไป for นอก
+                break;
+            }
+
+            *mT = *mT + *cD; 
+        }
+
+        printf("Month %d Total: %.2f\n", i + 1, *mT);
+
+        if (*mT >= 500.00) {
+            (*s)++; 
+        }
+    }
+}
