f4bf80743779680df8dfd4b29bfe82b28bcedfd2
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 01:58:44 +0700

    Update Main.cpp

--- 3.2/Main.cpp
+++ 3.2/Main.cpp
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-//กำหนดให้เป็นหมวดหมู่โดยมีเเยกย่อยข้างใน
-struct Student {
+struct Student
+{
     char name[50];
     int studentId;
     float score;
 };
 
-int main() {
+int main()
+{
     int N, i;
-    
-    
-    if (scanf("%d",&N) != 1) {
+
+    if (scanf("%d", &N) != 1)
+    {
         return 1;
     }
 
-     //ต้องเรียกเเม่เเบบ เเละตามด้วยชื่อที่ต้องการ
     struct Student students[N];
-    
-     //สามารถเข้าถึงได้โดยใช้ . เป็นตัวนำทาง
-    for (i = 0; i < N; i++) {
-        
-        if (scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3) {
+
+    for (i = 0; i < N; i++)
+    {
+
+        if (scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3)
+        {
             return 1;
         }
     }
-    
-     //สามารถเข้าถึงได้โดยใช้ . เป็นตัวนำทาง
-    for (i = 0; i < N; i++) {
-        printf("ID: %d,Score: %.2f,Name: %s\n",students[i].studentId, students[i].score, students[i].name);
+
+    for (i = 0; i < N; i++)
+    {
+        printf("ID: %d,Score: %.2f,Name: %s\n", students[i].studentId, students[i].score, students[i].name);
     }
 
     return 0;
