a3d785a851c3c5a0ddf3b9ce18f9749fd1ab12ae
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 01:51:26 +0700

    Update main.cpp

--- 4.10/main.cpp
+++ 4.10/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int ROWS = 3;
     int COLS = 3;
     int i, j;
@@ -8,8 +9,10 @@ int main() {
 
     printf("Enter %d integer numbers for the 3x3 array:\n", ROWS * COLS);
 
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
+    for (i = 0; i < ROWS; i++)
+    {
+        for (j = 0; j < COLS; j++)
+        {
             printf("Enter element for Row %d, Col %d: ", i, j);
             scanf("%d", &data[i][j]);
         }
@@ -19,15 +22,13 @@ int main() {
     printf("Array Size: %d Rows x %d Columns\n", ROWS, COLS);
     printf("Content (Table Format):\n");
 
-    // ROWS เเถว ลูปเเภว
-    for (i = 0; i < ROWS ; i++) {
-        //COLS ลูปหลัก
-        for (j = 0;j < COLS ; j++) {
-            printf("%d ",data[i][j]);
+    for (i = 0; i < ROWS; i++)
+    {
+        for (j = 0; j < COLS; j++)
+        {
+            printf("%d ", data[i][j]);
         }
-        // พอจบลูปหลักเเล้วก็ให้ขึ้นบรรทัดใหม่
         printf("\n");
     }
-
     return 0;
 }
