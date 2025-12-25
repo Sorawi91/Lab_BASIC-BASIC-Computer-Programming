eb0cf58e90c95c06d3fb1248bb3fd25c3f80fa83
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 01:59:42 +0700

    Update main.cpp

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-//กำหนดให้เป็นหมวดหมู่โดยมีเเยกย่อยข้างใน
-struct Product { 
+struct Product
+{
     char name[50];
     int productId;
     float price;
 };
 
-int main() {
-    //ต้องเรียกเเม่เเบบ เเละตามด้วยชื่อที่ต้องการ
-    struct Product item; 
+int main()
+{
 
-    //สามารถเข้าถึงได้โดยใช้ . เป็นตัวนำทาง
-    if (scanf("%d %f %s", &item.productId, &item.price, &item.name) != 3) {
+    struct Product item;
+
+    if (scanf("%d %f %s", &item.productId, &item.price, &item.name) != 3)
+    {
         return 1;
     }
-    
-    printf("ID: %d,Price: %.2f, Name: %s",item.productId,item.price,item.name);
+
+    printf("ID: %d,Price: %.2f, Name: %s", item.productId, item.price, item.name);
 
     return 0;
 }
