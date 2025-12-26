eb0efc8be3bb80756546ca0e075dc3eeea0d5c61
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 13:22:20 +0700

    Update main.cpp

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -9,14 +9,14 @@ struct Product
 
 int main()
 {
-
+//สร้างชื่อ struct item
     struct Product item;
-
+    //รับค่าจากผู้ใช้เเละเพิ่มเข้าไปที่ struct 
     if (scanf("%d %f %s", &item.productId, &item.price, &item.name) != 3)
     {
         return 1;
     }
-
+    //ปริ้นค่าที่อย่ใน struct
     printf("ID: %d,Price: %.2f, Name: %s", item.productId, item.price, item.name);
 
     return 0;
