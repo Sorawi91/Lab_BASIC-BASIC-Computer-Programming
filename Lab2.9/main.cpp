13e81c48a2c28cfa6cc867f78aa1c3545bf3fb5b
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 13:12:41 +0700

    Update main.cpp

--- Lab2.9/main.cpp
+++ Lab2.9/main.cpp
@@ -1,50 +1,59 @@
 #include <stdio.h>
 
+void Cases(int *Stocks , float *Fee , int *number,int *cmd , int *quantitys,float *totoal);
+
 int main()
 {
     int Stock;
     float PENALTY_FEE;
     float totalPenalties = 0.0;
-    int n, i, cmdCode, quantity;
+    int n, cmdCode, quantity;
 
     if (scanf("%d %f %d", &Stock, &PENALTY_FEE, &n) != 3)
     {
         return 0;
     }
+    
+    Cases(&Stock,&PENALTY_FEE,&n,&cmdCode,&quantity,&totalPenalties);
+    return 0;
+}
 
-    for (i = 0; i < n; i++)
+//ลดความซับซ้อนโดยการส่งค่า พารามิเตอร์เกือบทุกอย่างเพื่อให้มันประมวณผลใน fuunction เท่านั้นเลย
+void Cases(int *Stocks , float *Fee , int *number,int *cmd , int *quantitys,float *totoal){
+    
+    for (int i = 0; i < *number; i++)
     {
-        if (scanf("%d %d", &cmdCode, &quantity) != 2)
+        if (scanf("%d %d", &*cmd, &*quantitys) != 2)
         {
-            return 0;
+            break;
         }
-        switch (cmdCode)
+        switch (*cmd)
         {
         case 1:
-            Stock += quantity;
-            printf("Received %d units.\n", quantity);
+            *Stocks += *quantitys;
+            printf("Received %d units.\n", *quantitys);
             break;
 
         case 2:
-            if (quantity <= 0)
+            if (*quantitys <= 0)
             {
                 printf("Error: Quantity must be positive.\n");
             }
-            else if (quantity <= Stock)
+            else if (*quantitys <= *Stocks)
             {
-                Stock -= quantity;
-                printf("Shipped %d units.\n", quantity);
+                *Stocks -= *quantitys;
+                printf("Shipped %d units.\n", *quantitys);
             }
             else
             {
-                totalPenalties += PENALTY_FEE;
-                printf("FAILURE: Insufficent stock. PENALTY %.2f added.\n", PENALTY_FEE);
+                *totoal += *Fee;
+                printf("FAILURE: Insufficent stock. PENALTY %.2f added.\n", *Fee);
             }
             break;
 
         case 3:
-            printf("Current Stock: %d\n", Stock);
-            printf("Total Penalties: %.2f\n", totalPenalties);
+            printf("Current Stock: %d\n", *Stocks);
+            printf("Total Penalties: %.2f\n", *totoal);
             break;
 
         default:
@@ -52,5 +61,4 @@ int main()
             break;
         }
     }
-    return 0;
 }
